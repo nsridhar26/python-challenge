9b8a977dfb07c352f9ba9d471951439dfcb2f5d3
Nandini <43830438+nsridhar26@users.nopreply.github.com>
2018-12-16 18:34:47 -0500

    updated code

--- Pypoll/main.py
+++ Pypoll/main.py
@@ -1,20 +1,47 @@
 import os
 import csv
-import pandas as pd
+
 #relative path of CSV file
-file_to_load = os.path.join(Pypoll\election_data.csv)
+election_dt = os.path.join('Pypoll','election_data.csv')
+
+# Declared/set variable and set to 0 or list/dictionary
+total_votes = 0
+candidates = []
+percentage_votes = []
+number_votes = []
+win = []
+
 #open and read csv
-with open(file_to_load, newline="") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
+with open(election_dt, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
 # skip the header before the loop
-Next(reader, None)
-#print number of months in the data set
-fname = input("file_to_load: ")
+next(csvreader, None)
+#print number of rows in the data set
+fname = input("election_dt: ")
 num_lines = 0
 with open(fname, 'r') as f:
-    for line in f:
-        num_lines += 1
-            print("Total number of votes: ")
-            print(num_lines)
+    for row in f:
+        total_votes += 1
+        print("Total number of votes: ")
+        print(num_lines)
 #list of candidates who recieved votes
-unq.column(2)
+        if row["Candidate"] not in candidates:
+            
+            candidates.append(row["Candidate"])
+
+            number_votes[row["Candidate"]] = 1
+        else:
+            number_votes[row["Candidate"]] = number_votes[row["Candidate"]] + 1
+# identify total votes for every candidate and divide by total votes
+percentage_votes = number_votes/total_votes
+#find the winner 
+win = max(percentage_votes)
+
+# Add analysis into single results list
+print("Election Results")
+results.append(“Total Votes : ” + +str(round(total_votes)))
+results.append(str(Candidates, percentage_votes, number_votes)
+results.append(“Average Change :” +“$”+str(avg_Chng))
+results.append("Winner: " win)
+
+# write the analysis into file
